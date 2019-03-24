b59b7579059d89e12c54400ad2b254b23f76ab1b
Steven Woerpel <woerpels@uwplatt.edu>
2019-03-24 14:59:05 -0500

    restarting setup

--- SW/main.js
+++ SW/main.js
@@ -1,68 +1,57 @@
 
 
-require(['newsapi'], function (newsapi) {
-    //foo is now loaded.
-    console.log('chet')
-    console.log('newsapi', newsapi)
-    // const newsapi = new NewsAPI('89fb0cc1c7b847c8beabe9ab81d96994');
-    // console.log('chet')
-    // gather(newsapi)
-});
 
-
-
-//foo is now loaded.
-console.log('CHET')
-// const newsapi = new NewsAPI('89fb0cc1c7b847c8beabe9ab81d96994');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('89fb0cc1c7b847c8beabe9ab81d96994');
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
 newsapi.v2.topHeadlines({
-sources: 'bbc-news,the-verge',
-q: 'bitcoin',
-category: 'business',
-language: 'en',
-country: 'us'
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
 }).then(response => {
-console.log(response);
-/*
+  console.log(response);
+  /*
     {
-    status: "ok",
-    articles: [...]
+      status: "ok",
+      articles: [...]
     }
-*/
+  */
 });
 // To query /v2/everything
 // You must include at least one q, source, or domain
 newsapi.v2.everything({
-q: 'bitcoin',
-sources: 'bbc-news,the-verge',
-domains: 'bbc.co.uk, techcrunch.com',
-from: '2017-12-01',
-to: '2017-12-12',
-language: 'en',
-sortBy: 'relevancy',
-page: 2
+  q: 'bitcoin',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2017-12-01',
+  to: '2017-12-12',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 2
 }).then(response => {
-console.log(response);
-/*
+  console.log(response);
+  /*
     {
-    status: "ok",
-    articles: [...]
+      status: "ok",
+      articles: [...]
     }
-*/
+  */
 });
 // To query sources
 // All options are optional
 newsapi.v2.sources({
-category: 'technology',
-language: 'en',
-country: 'us'
+  category: 'technology',
+  language: 'en',
+  country: 'us'
 }).then(response => {
-console.log(response);
-/*
+  console.log(response);
+  /*
     {
-    status: "ok",
-    sources: [...]
+      status: "ok",
+      sources: [...]
     }
-*/
+  */
 });
